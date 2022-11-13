09f33ea2862e71ab5e1f69797c91b335768a2258
Tyler Cho <tymcho@gmail.com>
2022-11-12 22:03:37 -0800

    Basic display for collections and suggested recipes
    - Added 'pages' directory to hold all large-scale page JSX files
    - Added 'components' directory to hold all individual components to be
      placed on single page
    - Added Pages.jsx and Home.jsx, with Pages primarily to be used to
      display the different pages needed at different times, Home to be used
      for landing page display
    - Added Popular.jsx and Veggie.jsx as sliding-menu components to
      display different recipes; no recipe display functionality yet, only
      recipe name and photo
    - Added formatting information to index.css and index.html

--- cookbook-client/src/App.js
+++ cookbook-client/src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
+// IMPORTS USED:
+/*
+npm install framer-motion react-icons react-router-dom styled-components
+npm install @splidejs/react-splide
+*/
+
+import Pages from "./pages/Pages";
 
 function App() {
   return (
     <div className="App">
-      <body>
-        <p>hi anata</p>
-      </body>
+      <h1>Hello</h1>
+      <Pages />
     </div>
   );
 }

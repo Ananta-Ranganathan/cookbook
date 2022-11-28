31bb27d14c65d0624aef0d120cb26fe309cd0406
Ananta Ranganathan <anantadr@gmail.com>
2022-11-28 05:24:29 -0800

    post is working

--- cookbook-server/index.js
+++ cookbook-server/index.js
@@ -2,9 +2,13 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+
 const cors = require('cors')
 
 app.use(cors())
+
+const mongoose = require('mongoose')
 
 const dotenv = require('dotenv')
 
@@ -16,26 +20,56 @@ const uri = process.env.MONGODB_URI
 
 const port = process.env.PORT || 8000
 
+
 app.get('/', (req, res) => {
     res.send('default response from cookbook server');
 })
 
-app.get('/recipes/', (req, res) => {
+app.get('/recipes/:id', (req, res) => {
     MongoClient.connect(uri, function(err, db) {
-        let recipes = {}
+        let id = req.params.id
         if (err) throw err
         var dbo = db.db("test")
-        dbo.collection("recipes").find().toArray(function(err, result) {
-            if (err) throw err
-            for (i in result) {
-                recipes[result[i]["name"]] = result[i]["ingredients"]
-            }
-            res.json(recipes)
-            db.close()
-        })
+        res.json(dbo.collection("recipes").find({"_id": id}))
+        db.close()
     })
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+    const recipe = mongoose.model('Recipe', recipeSchema)
+    const newRecipe = new recipe({
+        name: req.body.name,
+        author: req.body.author,
+        ingredients: req.body.ingredients,
+        instructions: req.body.instructions,
+        notes: req.body.notes
+    })
+    mongoose.connect(uri)
+    newRecipe.save()
 })
 
 app.listen(port, () => {
     console.log(`app running at port ${port}`);
 })
+
+const recipeSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    ingredients: [String],
+    instructions: [String],
+    cuisine: [String],
+    time: { low: Number, high: Number },
+    skill: { easy: Boolean, medium: Boolean, hard: Boolean},
+    restrictions: { vegetarian: Boolean, gluten_free: Boolean, dairy_free: Boolean},
+    notes: [String]
+});
+
+const userSchema = new mongoose.Schema({
+   username: String,
+   password: String,
+   cuisines: [{ String: Number }],
+   time: Number,
+   skills: { easy: Number, medium: Number, hard: Number },
+   restrictions: { vegetarian: Number, gluten_free: Number, dairy_free: Number}
+});
